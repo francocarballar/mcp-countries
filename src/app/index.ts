36422226c8e91d43398423cad66889dbb54ae40e
Franco Carballar <francocarballar@gmail.com>
2025-05-05 15:25:15 -0300

    feat: :sparkles: implementa manejo de errores y traducción de mensajes en la aplicación Hono.
    - Se agregó la función `onError` para gestionar errores de manera centralizada, proporcionando respuestas JSON con mensajes traducidos según el idioma preferido del usuario.
    - Se mejoró la lógica de manejo de errores para registrar errores inesperados y traducir mensajes de error utilizando la función `getTranslatedMessage`.
    - Se actualizó la ruta principal para devolver un mensaje de bienvenida traducido, mejorando la experiencia del usuario al interactuar con la aplicación.
    - Estas adiciones optimizan la gestión de errores y la localización de mensajes, aumentando la mantenibilidad y accesibilidad del código.

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,15 @@
+// Types and interfaces
+import type { MessageKey } from '@/shared/types/message'
+
 // Modules and main functions
 import { Hono } from 'hono'
+import { HTTPException } from 'hono/http-exception'
 import { cache } from 'hono/cache'
 import { cors } from 'hono/cors'
 import { prettyJSON } from 'hono/pretty-json'
+
+// Utils
+import { getTranslatedMessage } from '@/shared/lib/i18n/getTranslatedMessage'
 
 const app = new Hono()
 
@@ -23,8 +30,57 @@ app.use(
   })
 )
 
+/**
+ * @function onError
+ * @description Handles errors in the application.
+ * @param {Error} err - The error object.
+ * @param {Context} c - The context object.
+ * @returns {Promise<Response>} A promise that resolves to a JSON response.
+ */
+app.onError((err, c) => {
+  let status: number = 500
+  let message: string
+  let logError: boolean = true
+
+  const acceptLanguage = c.req.header('accept-language')
+
+  if (err instanceof HTTPException) {
+    status = err.status
+    logError = status >= 500
+
+    const cause = err.cause as
+      | { key: MessageKey; params?: Record<string, string> }
+      | undefined
+
+    if (cause?.key) {
+      message = getTranslatedMessage(acceptLanguage, cause.key, cause.params)
+    } else {
+      message = err.message || 'An error occurred'
+    }
+  } else {
+    console.error('[UNEXPECTED ERROR]:', err)
+    message = getTranslatedMessage(acceptLanguage, 'internalServerError')
+  }
+
+  if (logError) {
+    console.error(`[ERROR - Status ${status}]:`, err)
+  }
+
+  const responsePayload = {
+    error: {
+      status,
+      message
+    }
+  }
+
+  return c.json(responsePayload, status as any)
+})
+
 app.get('/', c => {
-  return c.text('Hello Hono!')
+  const acceptLanguage = c.req.header('accept-language')
+  const message = getTranslatedMessage(acceptLanguage, 'welcome')
+
+  return c.text(message)
 })
 
 export default app
