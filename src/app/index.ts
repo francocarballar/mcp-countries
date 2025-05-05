d8218c9ae06f96fe4f6616d089bf8e3c09bc5dcf
Franco Carballar <francocarballar@gmail.com>
2025-05-05 15:17:30 -0300

    feat: :sparkles: agrega middlewares para manejo de CORS, caché y formato JSON en la aplicación Hono.
    - Se implementaron middlewares para habilitar CORS, aplicar caché y formatear respuestas en JSON.
    - Se mejoró la estructura del archivo src/app/index.ts para incluir estas funcionalidades, optimizando la respuesta del servidor.

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,29 @@
+// Modules and main functions
 import { Hono } from 'hono'
+import { cache } from 'hono/cache'
+import { cors } from 'hono/cors'
+import { prettyJSON } from 'hono/pretty-json'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+// Middlewares
+app.use('*', prettyJSON())
+app.use(
+  '*',
+  cors({
+    origin: '*'
+  })
+)
+app.use(
+  '*',
+  cache({
+    cacheName: 'mcp-countries-cache',
+    cacheControl: 'public, max-age=604800, s-maxage=604800',
+    vary: ['Accept-Language']
+  })
+)
+
+app.get('/', c => {
   return c.text('Hello Hono!')
 })
 
