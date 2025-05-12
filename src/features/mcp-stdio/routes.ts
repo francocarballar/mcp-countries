717d9145c633b075a42dc180b6c368a0955c3dba
Franco Carballar <francocarballar@gmail.com>
2025-05-11 23:57:46 -0300

    feat: :sparkles: agrega rutas para el servidor MCP-STDIO.
    - Se creó el archivo `routes.ts` que define la función principal del servidor MCP-STDIO, integrando la obtención de todos los países a través de la API y estableciendo la conexión con el servidor.
    - Se implementó un sistema de logging para registrar errores, mejorando la capacidad de seguimiento y la mantenibilidad del código.
    - Esta adición establece la infraestructura necesaria para el funcionamiento del servidor MCP-STDIO, facilitando futuras extensiones y mejoras en la gestión de datos.

---
+++ src/features/mcp-stdio/routes.ts
@@ -0,0 +1,17 @@
+// Services
+import { getAllCountries } from '@/shared/lib/tools/getAllCountries'
+
+// Utils
+import { SERVER, TRANSPORT } from '@/shared/lib/utils/mcp'
+import { createMcpLogger } from '@/shared/lib/utils/logger'
+
+async function main () {
+  getAllCountries(SERVER)
+  await SERVER.connect(TRANSPORT)
+}
+
+main().catch(error => {
+  const logger = createMcpLogger('[MCP-STDIO]')
+  logger.error('Error in MCP stdio server:', error)
+  process.exit(1)
+})
