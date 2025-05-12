83dfb38ef880bd78537f7dc3633020dae852b21b
Franco Carballar <francocarballar@gmail.com>
2025-05-11 23:49:27 -0300

    feat: :sparkles: agrega constantes para la configuración de la API.
    - Se creó el archivo `api.ts` que define constantes relacionadas con la configuración de la API, incluyendo `API_URL`, `API_VERSION`, `API_URL_WITH_VERSION` y `API_ENDPOINTS`.
    - Estas adiciones mejoran la organización y la claridad del código, facilitando el acceso y la gestión de las configuraciones de la API en toda la aplicación.
    - La implementación de estas constantes sienta las bases para un manejo más eficiente de las interacciones con la API, aumentando la mantenibilidad del código

---
+++ src/shared/config/constants/api.ts
@@ -0,0 +1,29 @@
+/**
+ * @constant API_URL
+ * @description URL of the API
+ */
+export const API_URL = 'https://restcountries.mcp-blockchain-metadata.workers.dev'
+
+/**
+ * @constant API_VERSION
+ * @description Version of the API
+ */
+export const API_VERSION = 'v1'
+
+/**
+ * @constant API_URL_WITH_VERSION
+ * @description URL of the API with the version
+ */
+export const API_URL_WITH_VERSION = `${API_URL}/${API_VERSION}`
+
+
+/**
+ * @constant API_ENDPOINTS  
+ * @description Endpoints of the API
+ */
+export const API_ENDPOINTS = {
+  ALL: '/all',
+  BY_NAME: '/name',
+  REGIONS: '/regions',
+  BY_REGION: '/region',
+}
