dea87ff5a7772da5dff361bf11873af8f3b5d47e
Franco Carballar <francocarballar@gmail.com>
2025-05-12 00:01:32 -0300

    feat: :sparkles: agrega interfaz McpErrorDetails para manejo de errores.
    - Se creó el archivo `errors.d.ts` que define la interfaz `McpErrorDetails`, la cual especifica la estructura de los detalles de un error, incluyendo un código numérico y un mensaje descriptivo.
    - Esta adición mejora la claridad y la consistencia en el manejo de errores dentro de la aplicación, facilitando la identificación y gestión de errores en el sistema.
    - La implementación de esta interfaz sienta las bases para un manejo más robusto y estructurado de los errores, aumentando la mantenibilidad del código.

---
+++ src/shared/types/lib/utils/errors.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @interface McpErrorDetails
+ * @description Details of the error
+ * @property {number} code - The code of the error
+ * @property {string} message - The message of the error
+ */
+export interface McpErrorDetails {
+  code: number
+  message: string
+}
